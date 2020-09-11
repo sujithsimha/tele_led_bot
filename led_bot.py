f0142c86dedc722786380591e5f8da4d8e1b65a6
sujithsimha <70851132+sujithsimha@users.noreply.github.com>
2020-09-11 17:27:16 +0530

    Update led_bot.py

--- led_bot.py
+++ led_bot.py
@@ -3,7 +3,7 @@ from Adafruit_IO import Client, Feed,Data
 import requests #gets data from cloud
 import os
 
-name = "sujithsimha"
+name = os.getenv("ADAFRUIT_IO_USERNAME")
 key = os.getenv("ADAFRUIT_IO_KEY")
 colour={0:'🔴',
         1:'🟢'}
